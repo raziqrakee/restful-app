4724e14217cd7b3ec63444b501585ed522fcf3a9
Raziq Rakee <97151754+raziqrakee@users.noreply.github.com>
2024-06-13 00:25:11 +0800

    [CREATE] edit and delete user using AXIOS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./layout/Navbar";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddUser from "./users/AddUser";
+import EditUser from "./users/EditUser";
+import ViewUser from "./users/ViewUser";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/addUser" element={<AddUser />} />
+          <Route path="/editUser/:id" element={<EditUser />} />
+          <Route path="/viewUser/:id" element={<ViewUser />} />
         </Routes>
       </Router>
     </div>

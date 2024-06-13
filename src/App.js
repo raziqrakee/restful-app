f6bbbfc38b1f24fd0c25d7dc24f86f7d8b6955d0
Nurin Khairina <nurinkhairina@graduate.utm.my>
2024-06-13 14:03:57 +0800

    added Welcome and About page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./layout/Navbar";
 import Home from "./pages/Home";
+import Welcome from './pages/Welcome';
+import About from './pages/About';
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddUser from "./users/AddUser";
 import EditUser from "./users/EditUser";
@@ -14,10 +17,12 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Welcome />} />
+          <Route path="/Home" element={<Home />} />
           <Route path="/addUser" element={<AddUser />} />
           <Route path="/editUser/:id" element={<EditUser />} />
           <Route path="/viewUser/:id" element={<ViewUser />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </Router>
     </div>
